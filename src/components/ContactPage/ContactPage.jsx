6e339fc25aa36c1775819a4d1cdedb0ef8cb6fb8
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-07-29 19:20:15 -0500

    Changes to the contact page

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -7,59 +7,62 @@ function ContactPage() {
 
   //! What displays
   return (
-    <div className="container">
+    <center>
+      <div className="contact">
+        <div>
+          <Typography variant="h3"
+            sx={{ textAlign: "center", }}>
+            Contact
+          </Typography>
 
-      <Typography variant="h3"
-        sx={{ textAlign: "center", }}>
-        Contact
-      </Typography>
+          <br />
 
-      <br />
+          {/* Contact info */}
+          <Typography variant='h6'>
 
-      {/* Contact info */}
-      <Typography variant='h6'>
+            Send us an e-mail: <br />
+            <a href="mailto:goblackheart@comcast.net">Goblackheart@comcast.NET</a>
+            <br /> <hr />
 
-        Send us an e-mail: <br />
-        <a href="mailto:goblackheart@comcast.net">Goblackheart@comcast.NET</a>
+            Check out our website: <br />
+            {/* Opens Black Heart's website in a new tab */}
+            <a href={"https:blackheartmodels.com"} target="_blank"> Www.blackheartmodels.com</a>
+            <br /> <hr />
+
+            Follow us on social media: <br />
+            {/* Opens Black Heart's LinkTree in a new tab */}
+            <a href={"https://linktr.ee/blackheartmodels"} target="_blank"> Visit our Linktree </a>
+          </Typography>
+        </div>
+
+        <br />
+
+        <div className='contactPhoto'>
+          <img src="images/blackheart-postcard.jpg" alt="Black Heart Postcard" />
+        </div>
+
         <br /> <hr />
 
-        Check out our website: <br />
-        {/* Opens Black Heart's website in a new tab */}
-        <a href={"https:blackheartmodels.com"} target="_blank"> Www.blackheartmodels.com</a>
-        <br /> <hr />
+        {/* Commission info */}
+        <Typography variant="h5" sx={{ textAlign: 'center' }}> Commission process: </Typography>
 
-        Follow us on social media: <br />
-        {/* Opens Black Heart's LinkTree in a new tab */}
-        <a href={"https://linktr.ee/blackheartmodels"} target="_blank"> Visit our Linktree </a>
-      </Typography>
+        <br />
 
-      <br />
+        <Typography> If hiring us for a project, we will do our best to fulfill every request.
+          We will provide in-progress photos for approval and make changes as needed.
 
-      <center>
-        <img src="images/blackheart-postcard.jpg" alt="Black Heart Postcard" />
-      </center>
+          <br /> <br />
 
-      <br /> <hr />
+          Every kit is airbrushed and hand-painted. We spend several hours on each piece
+          to ensure that you are proud to display our work.
 
-      {/* Commission info */}
-      <Typography variant="h5" sx={{ textAlign: 'center' }}> Commission process: </Typography>
+          <br /> <br />
 
-      <br />
+          Please email us for a quote. Serious inquiries only please, thank you!
+        </Typography>
 
-      <Typography> If hiring us for a project, we will do our best to fulfill every request.
-        We will provide in-progress photos for approval and make changes as needed.
-
-        <br /> <br />
-
-        Every kit is airbrushed and hand-painted. We spend several hours on each piece
-        to ensure that you are proud to display our work.
-
-        <br /> <br />
-
-        Please email us for a quote. Serious inquiries only please, thank you!
-      </Typography>
-
-    </div>
+      </div>
+    </center>
   );
 } // End ContactPage()
 
